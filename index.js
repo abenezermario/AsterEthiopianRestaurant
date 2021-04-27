92d995dc116a7fbf2966835d5f0e73672c3681a1
Abenezer Mario <abenezer.mario@go.mnstate.edu>
2021-04-27 18:29:15 -0500

    updatedHomePage

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
+// $(document).ready(function() {
+//     $("button").on("click", function() {
+//       if ($(this).hasClass("nav-button")) {
+//         $("nav div").addClass("show");
+//       } else if ($(this).hasClass("exit-menu")) {
+//         $("nav div").removeClass("show");
+//       } else if ($(this).hasClass("to-top")) {
+//         $("html,body").animate({ scrollTop: 0 }, "slow");
+//       }
+//     });
+  
+//     AOS.init({
+//       duration: 1800,
+//       easing: "ease"
+//     });
+//   });
+//   $('.gn-icon-menu').hover(function(){
+//     $('.gn-menu-wrapper').toggleClass('gn-open-part');
+//   });
+  
+//   $('.gn-menu-wrapper').hover(function(){
+//     $(this).toggleClass('gn-open-all');
+//   })
+// by: George Olaru https://dribbble.com/shots/1560982-Rosa-Restaurant-Website/attachments/239212
+
 $(document).ready(function() {
-    $("button").on("click", function() {
-      if ($(this).hasClass("nav-button")) {
-        $("nav div").addClass("show");
-      } else if ($(this).hasClass("exit-menu")) {
-        $("nav div").removeClass("show");
-      } else if ($(this).hasClass("to-top")) {
-        $("html,body").animate({ scrollTop: 0 }, "slow");
-      }
-    });
   
-    AOS.init({
-      duration: 1800,
-      easing: "ease"
-    });
+  $('button').on('click', function() {
+    if($(this).hasClass('nav-button')) {
+      $('nav div').addClass('show');
+    } else if($(this).hasClass('exit-menu')) {
+      $('nav div').removeClass('show');
+    } 
+    else if($(this).hasClass('to-top')) {
+      $('html,body').animate({scrollTop:0}, 'slow');
+    }
   });
-  $('.gn-icon-menu').hover(function(){
-    $('.gn-menu-wrapper').toggleClass('gn-open-part');
+
+  AOS.init({      
+        duration: 1800,
+    easing: 'ease'
   });
-  
-  $('.gn-menu-wrapper').hover(function(){
-    $(this).toggleClass('gn-open-all');
-  })
+   
+})
